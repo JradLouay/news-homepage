f3f3d58f5ce6e05855c16790481bb45cbfab7928
Louay Jrad <jrad.louay@outlook.com>
2024-02-07 17:15:32 +0100

    finish mobile design and add mobile menu

---
+++ toggleMenu.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("mobile-menu").style.width = "256px";
+  document.getElementById("overlay").style.display = "block";
+}
+
+function closeNav() {
+  document.getElementById("mobile-menu").style.width = "0";
+  document.getElementById("overlay").style.display = "none";
+}
